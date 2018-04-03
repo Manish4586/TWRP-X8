bcfe8b5484376cd7002a87748d6439587539ba1a
Manish Sarkar <Manish.n.manish45@gmail.com>
2018-04-03 21:54:14 +0530

    Update flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,57 +1,66 @@
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := Mediatek
-
 # Platform
 TARGET_BOARD_PLATFORM := mt6592
+TARGET_NO_BOOTLOADER := true
 
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
 
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-# Kernel
-TARGET_PREBUILT_KERNEL := $(LOCAL_DIR)/kernel
-BOARD_KERNEL_CMDLINE := 
+# Kernel 
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+BOARD_KERNEL_CMDLINE :=
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_DIR)/mkbootimg.mk
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 
-# USB Mounting
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun0/file
-
-# Recovery
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_HAS_LARGE_FILESYSTEM := true
+# make_ext4fs requires numbers in dec format
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 13485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1237319680
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Device-Specific
-DEVICE_RESOLUTION := 720 x 1280
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+TARGET_KMODULES := true
 
-# TWRP-Specific
-TARGET_RECOVERY_FSTAB := device/Lava/irisX8/recovery/etc/twrp.fstab
-BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_THEME := landspace_hdpi
-TW_EXCLUDE_MTP := true
+# Assert
+TARGET_OTA_ASSERT_DEVICE := irisX8
+
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
+
+# TWRP stuff
+TW_THEME := portrait_hdpi
+TW_SDEXT_NO_EXT4 := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/power_supply/battery/batt_temp"
 RECOVERY_SDCARD_ON_DATA := true
-TW_INTERNAL_STORAGE_PATH := "/data/media"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+BOARD_HAS_LARGE_FILESYSTEM := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_THEME := portrait_hdpi
+TW_INCLUDE_FB2PNG := true
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_NO_REBOOT_BOOTLOADER := true
-TW_NO_CPU_TEMP := true
-TW_INCLUDE_CRYPTO := true
-TW_MAX_BRIGHTNESS := 255
+TW_USB_STORAGE := true
+TW_EXCLUDE_MTP := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_INCLUDE_JB_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p7"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/etc/twrp.fstab
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/etc/twrp.fstab:root/twrp.fstab
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/etc/recovery.fstab:root/recovery.fstab
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/etc/fstab.mt6592:root/fstab.mt6592
--- omni_irisX8.mk
+++ omni_irisX8.mk
@@ -1,17 +1,21 @@
+# Release name
+PRODUCT_RELEASE_NAME := irisX8
+
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := omni_irisX8
-PRODUCT_DEVICE := Iris X8
-PRODUCT_BRAND := iris
-PRODUCT_MANUFACTURER := Lava
-PRODUCT_MODEL := irisX8
+PRODUCT_COPY_FILES += \ 
+    $(LOCAL_PATH)/recovery/etc/recovery.fstab:root/recovery.fstab
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="irisX8"
-
-# Correct bootanimation size for the screen
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/root/twrp.fstab.rc:root/twrp.fstab.rc \
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Iris X8
+PRODUCT_NAME := omni_irisX8
+PRODUCT_BRAND := iris
+PRODUCT_MODEL := irisX8
+PRODUCT_MANUFACTURER := Lava
+
+# Available languages
+PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG
